cb0e229f0d1684c443c99cf75185db99d75634d7
Zyfgoup <619122012@qq.com>
2020-05-08 14:27:32 +0800

    测试新增

---
+++ CRUD_Demo/src/test/java/com/zyfgoup/AddTest.java
@@ -0,0 +1,12 @@
+package com.zyfgoup;
+
+/**
+ * @Author Zyfgoup
+ * @Date 2020/5/8 14:26
+ * @Description
+ */
+public class AddTest {
+    public static void main(String[] args) {
+        System.out.println("测试新增文件");
+    }
+}
